2e412f7ce3581248d80dc865a738fc3fd8a116fd
Minhaj Sadik <minhajsadik@Minhajs-MacBook-Air.local>
2024-03-27 23:03:56 +0600

    [DONE] optimise code and efficiency

--- services/mail.service.js
+++ services/mail.service.js
@@ -34,7 +34,7 @@ class Service {
 
     generateTrackingPixelUrl(emailID) {
         // return crypto.randomBytes(16).toString('hex');
-        return `https://emailer-k7j7hvszs-minhajsadiks-projects.vercel.app/api/v1/track/open?emailID=${emailID}`;
+        return `https://emailer-mcwhr0xx7-realtime-pizza-app.vercel.app/api/v1/track/open?emailID=${emailID}`;
     }
 
     // async mailLog(emailID) {
