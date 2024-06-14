e03180944f6f40088a2fda72f5c54526f1484afa
MinhajSadik <minhajosadik@gmail.com>
2024-06-14 19:41:19 +0600

    [RESOLVING] MongoNetworkError: connect ECONNREFUSED 127.0.0.1:27017

--- configs/database.config.js
+++ configs/database.config.js
@@ -6,7 +6,10 @@ const DB_URL = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
 
 async function connectDB(){
     try {
-        mongoose.connect(DB_URL);
+        mongoose.connect(DB_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
         console.log(`Database Connected Successfully...`);
       } catch (error) {
         console.error(error.message);
