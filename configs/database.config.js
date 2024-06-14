131418b46af612315bd3b54ddac23b87a4bb939e
MinhajSadik <minhajosadik@gmail.com>
2024-06-14 19:46:46 +0600

    [RESOLVING] MongoNetworkError: connect ECONNREFUSED 127.0.0.1:27017

--- configs/database.config.js
+++ configs/database.config.js
@@ -4,25 +4,26 @@ dotenv.config()
 
 const DB_URL = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`;
 
-// async function connectDB(){
-//     try {
-//         mongoose.connect(DB_URL, {
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true,
-//         });
-//         console.log(`Database Connected Successfully...`);
-//       } catch (error) {
-//         console.error(error.message);
-//       }
-// }
+async function connectDB(){
+    // try {
+    //     mongoose.connect(DB_URL, {
+    //       useNewUrlParser: true,
+    //       useUnifiedTopology: true,
+    //     });
+    //     console.log(`Database Connected Successfully...`);
+    //   } catch (error) {
+    //     console.error(error.message);
+    //   }
+    mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(() => {
+      console.log('Connected to MongoDB');
+    }).catch((err) => {
+      console.error('Failed to connect to MongoDB', err);
+    });
+}
 
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Failed to connect to MongoDB', err);
-});
+
 
 export default connectDB;
